c86400f24549c0281f3683c097bb1d62b49cbf13
Jeffrey King <jdking33@aol.com>
2016-07-11 08:16:50 -0700

    Submit Assignment 3-Get Me

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,4 +8,9 @@ class Kele
     puts "Email/password combination not found. Please try again." if response.code != 200
     @authorization_token = response["auth_token"]
   end
+
+  def get_me
+    response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @authorization_token} )
+    @student_data = JSON.parse(response.body, symbolize_names: true)
+  end
 end
